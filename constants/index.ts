0bae63f610d96b43a76bb9f1bd8106fae184de28
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2024-12-31 11:47:51 +0400

    develop: Adding features texts

--- constants/index.ts
+++ constants/index.ts
@@ -26,3 +26,21 @@ export const TEAM = [
     image: "/images/vitaly-gariev-u6W_mKV6xBI-unsplash.jpg",
   },
 ];
+
+export const FEATURES = [
+  {
+    title: "Real-Time Data",
+    description:
+      "Stay informed with live updates on cryptocurrency prices, trading volumes, and market trends. Our platform delivers the most accurate and up-to-the-minute data, enabling you to respond swiftly to market changes and seize opportunities as they arise. With real-time monitoring, you'll always be a step ahead in the fast-paced crypto world.",
+  },
+  {
+    title: "Market Comparisons",
+    description:
+      "Navigate the cryptocurrency landscape with ease by comparing prices, trading pairs, and liquidity across multiple exchanges. Our advanced comparison tools empower you to identify the best opportunities for trading and investing, ensuring you maximize your returns and make smarter decisions.",
+  },
+  {
+    title: "Detailed Insights",
+    description:
+      "Go beyond the surface with in-depth historical data, interactive charts, and advanced metrics. Analyze long-term trends, assess market performance, and uncover insights that give you a comprehensive understanding of the cryptocurrency market. Whether you’re a data enthusiast or a professional trader, our platform offers the tools you need for deep analysis and strategy building.",
+  },
+];
